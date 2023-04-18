e190126d95b50378398b2bdcd099361f92a370cf
Marvelbiz <Emmanuellawal04@gmail.com>
2023-04-18 18:49:55 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+function Add(a, b){
+   document.getElementById('add').innerHTML=54 + 98;
+
+ }
+
+ function Sub(a, b){
+    document.getElementById('sub').innerHTML=100 - 79;
+ 
+  }
+
+  function Mul(a, b){
+    document.getElementById('multi').innerHTML=40 * 9;
+ 
+  }
+
+  function Div(a, b){
+    document.getElementById('div').innerHTML=200 /50;
+ 
+  }
+
+  function Mod(a, b){
+    document.getElementById('mod').innerHTML=400 % 50;
+ 
+  }
+
+  function Exp(a, b){
+    document.getElementById('exp').innerHTML=5 ** 5;
+ 
+  }
